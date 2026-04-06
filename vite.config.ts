d50ee925f75e8229d730af3c808b9bd15acad422
Smolle <romayacovlev@gmail.com>
2026-04-06 20:41:02 +0400

    ghPages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/HaremClickerInfinity/" : "./",
+  base: "/HaremClickerInfinity/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
